6df53eeba9a1915b40a5fe6eb264520ea1be58d2
Nayte91 <nayte91@gmail.com>
2021-05-15 00:11:05 +0200

    Base design of navigation

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navigation from "../components/Navigation";
 
 const Contact = () => {
     return (
         <div>
+            <Navigation />
             <h1>Hello Contact</h1>
         </div>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navigation from "../components/Navigation";
 
 const Home = () => {
     return (
-        <div>
-          Home here
-        </div>
+        <>
+          <Navigation />
+        </>
     );
 };
 
