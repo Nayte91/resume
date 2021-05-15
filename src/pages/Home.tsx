5f34747c25baac1bc93a436fd13644cb37fe392f
Nayte91 <nayte91@gmail.com>
2021-05-15 15:57:56 +0200

    created styles for each page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,15 @@ const Home = () => {
     return (
         <div id="home">
           <Navigation />
+            <div id="home-content">
+                <div className="content">
+                    <h1>Julien Robic</h1>
+                    <h2>Développeur web</h2>
+                    <div className="pdf">
+                        <a href="./media/CV.pdf">Téléchargez mon CV</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
