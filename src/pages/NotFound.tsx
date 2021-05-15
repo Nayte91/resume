855aa0a7b40bbdef132c5eb606c223d387feaaa5
Nayte91 <nayte91@gmail.com>
2021-05-15 17:19:53 +0200

    not found page stylized

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            Error 404
+        <div id="not-found">
+            <div id="not-found-content">
+                <h3>Désolé, cette page n'existe pas !</h3>
+                <NavLink exact to="/" >
+                    <i className="fas fa-home"><span>Accueil</span></i>
+                </NavLink>
+            </div>
         </div>
     );
 };
